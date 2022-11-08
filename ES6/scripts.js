c31956018a6a3aed5926ed6d816497b647652d3f
G.Venancio <gabrielly_venancio@live.com>
2022-11-08 14:49:31 -0300

    aula - arrow function

--- ES6/scripts.js
+++ ES6/scripts.js
@@ -31,3 +31,15 @@ console.log(a)
  }
 
  const name = 'Gabrielly'
+
+ // aula - arrow function
+ const sum = function sum  (a,b) {
+    return a + b
+ }
+
+ const arrowSum = (a,b) => a + b
+
+
+ console.log(sum(5, 5))
+ console.log(arrowSum(5,5))
+
