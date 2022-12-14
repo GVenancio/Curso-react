c85f3d03f82c4b4364b017cfe9c133239f25363c
G.Venancio <gabrielly_venancio@live.com>
2022-12-14 08:39:10 -0300

    Update scripts.js

--- ES6/scripts.js
+++ ES6/scripts.js
@@ -43,3 +43,34 @@ console.log(a)
  console.log(sum(5, 5))
  console.log(arrowSum(5,5))
 
+const greeting = (name) => {
+   if(name) {
+      return 'Olá' + name + '!'
+   } else{
+      return 'Olá!'
+   }
+}
+console.log (greeting ('Gabrielly'))
+console.log(greeting())
+
+const testeArrow = () => console.log('testou!')
+testeArrow()
+
+const user = {
+
+   name: 'Gaby',
+   sayUserName(){
+      var self = this
+      setTimeout (function (){
+         console.log(self)
+         console.log('Username:' + self.name)
+      }, 500)
+   },
+   sayUserNameArrow(){
+      setTimeout(() =>{
+         console.log('Username:' + this.name)
+      },700)
+   }
+}
+
+user.sayUserName()
